7068b78de530d96f6b13eadb13f63b9b9b0d5db9
aPusio <S@sz@12345>
2020-12-07 21:59:32 +0100

    Initial commit

---
+++ src/test/java/com/pusio/example/budgetingaid/BudgetingAidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pusio.example.budgetingaid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetingAidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
